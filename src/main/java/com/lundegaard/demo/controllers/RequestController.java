0b2b101e853b8955b35836f32a86f76d92ecdd74
ondra <ondrej.mazanec@gmail.com>
2020-10-14 19:16:02 +0200

    refactor index

--- src/main/java/com/lundegaard/demo/controllers/RequestController.java
+++ src/main/java/com/lundegaard/demo/controllers/RequestController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
@@ -41,6 +42,13 @@ public class RequestController {
         requestService.save(request);
 
         model.addAttribute("requests", requestService.getRequestList());
-        return "index";
+        return "request-list";
+    }
+
+    // handler method for showing request list and link for adding a new request
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("requests", requestService.getRequestList());
+        return "request-list";
     }
 }
